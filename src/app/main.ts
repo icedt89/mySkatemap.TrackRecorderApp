336b3f47e44402008d1c4738d24c56b14ecf8cbc
Jan Hafner <icedt89@gmail.com>
2017-04-01 11:18:01 +0200

    .

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,4 +1,7 @@
+import { enableProdMode } from "@angular/core";
 import { AppModule } from "./app.module";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+enableProdMode();
