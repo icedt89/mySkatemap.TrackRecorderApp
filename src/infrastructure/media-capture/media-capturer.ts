8aa10bdb705e39be777014c67819a847b44c74ba
Jan Hafner <icedt89@gmail.com>
2017-04-17 11:24:49 +0200

    .

--- src/infrastructure/media-capture/media-capturer.ts
+++ src/infrastructure/media-capture/media-capturer.ts
@@ -9,15 +9,18 @@ export class MediaCapturer {
 
     public captureCameraImage(): Promise<CapturedMediaResult> {
         return this.camera.getPicture(<CameraOptions>{
-            destinationType: 0,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            allowEdit: false,
+            quality: 100,
             correctOrientation: true
         }).then((result: string) => new CapturedMediaResult(result));
     }
 
     public selectLibraryImage(): Promise<CapturedMediaResult> {
         return this.camera.getPicture(<CameraOptions>{
-            destinationType: 0,
-            sourceType: 0,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            allowEdit: false,
+            sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
             correctOrientation: true
         }).then((result: string) => new CapturedMediaResult(result));
     }
